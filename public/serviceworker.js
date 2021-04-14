501fe805bc98f33284e509d6cee914e58ae6aec4
User <sebastian.mariani.94@gmail.com>
2021-04-14 15:52:30 +0100

    adding availability of pwa

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -15,9 +15,26 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-    
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
 });
 
 self.addEventListener('activate', (event) => {
-    
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)){
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    )
 });
