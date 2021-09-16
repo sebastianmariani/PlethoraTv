2d8675cc0f7b79eed69cbb4d3c9a5252519b77ae
Sebastian Mariani <sebastian.mariani.94@gmail.com>
2021-09-16 16:47:08 +0100

    changing color searchBar and fix width li on rightNavbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,58 +1,81 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import MovieElement from './MovieElement';
-import TvElement from './TvElement';
-import ActorElement from './ActorElement';
-import UnfoundElement from './UnfoundElement';
+import MovieElement from "./MovieElement";
+import TvElement from "./TvElement";
+import ActorElement from "./ActorElement";
+import UnfoundElement from "./UnfoundElement";
 
 const SearchBar = (props) => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [results, setResults] = useState(['']);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([""]);
 
-    const onSearchHandler = (e) => {
-        e.preventDefault();
-        setSearchTerm(e.target.value);
+  const onSearchHandler = (e) => {
+    e.preventDefault();
+    setSearchTerm(e.target.value);
 
-        fetch(`https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${e.target.value}&page=1&include_adult=false`)
-        .then(res => res.json())
-        .then(data => {
-            if (!data.errors) {
-                setResults(data.results);
-            } else {
-                setResults([])
-            }
-        })
-    }
-    
-    return (
-        <div className="searchbar">
-            <div className="search">
-                <input 
-                    autoFocus
-                    type="text" 
-                    value={searchTerm}
-                    onChange= { onSearchHandler }/>
-                <span onClick={props.toggleFocus}>close</span>
-            </div>
-            {results.length > 0 ? (
-            <div className="listResult">
-                {results.map((result) => {
-                    if( result.media_type === "movie") {
-                        return <MovieElement toggleFocus={props.toggleFocus} result={result} key={result.id}/>
-                    } else if ( result.media_type === "tv"){
-                        return <TvElement toggleFocus={props.toggleFocus} result={result}  key={result.id}/>
-                    } else if ( result.media_type === "person"){
-                        return <ActorElement toggleFocus={props.toggleFocus} result={result} key={result.id}/>
-                    } else {
-                        return <span></span>
-                    }
-                })}
-            </div>
-            ) : (
-            <div className="listResult"><UnfoundElement /></div>
-            )}
-        </div>
+    fetch(
+      `https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${e.target.value}&page=1&include_adult=false`
     )
-}
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.errors) {
+          setResults(data.results);
+        } else {
+          setResults([]);
+        }
+      });
+  };
 
-export default SearchBar
+  return (
+    <div className="searchbar">
+      <div className="search">
+        <input
+          autoFocus
+          type="text"
+          value={searchTerm}
+          onChange={onSearchHandler}
+        />
+        <span onClick={props.toggleFocus}>close</span>
+      </div>
+      {results.length > 0 ? (
+        <div className="listResult">
+          {results.map((result) => {
+            if (result.media_type === "movie") {
+              return (
+                <MovieElement
+                  toggleFocus={props.toggleFocus}
+                  result={result}
+                  key={result.id}
+                />
+              );
+            } else if (result.media_type === "tv") {
+              return (
+                <TvElement
+                  toggleFocus={props.toggleFocus}
+                  result={result}
+                  key={result.id}
+                />
+              );
+            } else if (result.media_type === "person") {
+              return (
+                <ActorElement
+                  toggleFocus={props.toggleFocus}
+                  result={result}
+                  key={result.id}
+                />
+              );
+            } else {
+              return <span></span>;
+            }
+          })}
+        </div>
+      ) : (
+        <div className="listResult">
+          <UnfoundElement />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SearchBar;
